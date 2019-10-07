820396af06fdab57ed29431ecf7e32f3320fdcfd
garmoni <opasyura@gmail.com>
2019-10-07 09:38:42 +0300

    OOP refactoring

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import state, {addPost, subscribe, updateNewPostText} from './redux/state';
+import store from './redux/state';
 import {BrowserRouter} from "react-router-dom";
 
 let renderEntireTree = (state) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App appState={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+            <App appState={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}/>
         </BrowserRouter>, document.getElementById('root'));
 }
 
-renderEntireTree(state);
-subscribe(renderEntireTree);
+renderEntireTree(store.getState());
+store.subscribe(renderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,62 +1,66 @@
-let renderEntireTree = () => {
-    console.log('State chenged')
-}
-let state = {
-    profilePage: {
-        myPostData: [
-            {id: 1, message: "Hi, how are you?", likenum: 15},
-            {id: 2, message: "It's my first post", likenum: 10},
-            {id: 3, message: "It's my first post", likenum: 10},
-            {id: 4, message: "It's my first post", likenum: 10}
-        ],
-        newPostText: ''
+let store = {
+    _state: {
+        profilePage: {
+            myPostData: [
+                {id: 1, message: "Hi, how are you?", likenum: 15},
+                {id: 2, message: "It's my first post", likenum: 10},
+                {id: 3, message: "It's my first post", likenum: 10},
+                {id: 4, message: "It's my first post", likenum: 10}
+            ],
+            newPostText: ''
 
+        },
+        dialogsPage: {
+            dialogsData: [
+                {id: 1, name: 'Dimitry', image: 'guy-6.jpg'},
+                {id: 2, name: 'Mary', image: 'woman-5.jpg'},
+                {id: 3, name: 'John', image: 'guy-4.jpg'},
+                {id: 4, name: 'Ria', image: 'woman-6.jpg'},
+                {id: 5, name: 'Jake', image: 'guy-3.jpg'}
+            ],
+            messages: [
+                {id: 1, message: "Hi Mary, Is it ok if we schedule the meeting tomorrow?", avamessage:"guy-6.jpg"},
+                {id: 2, message: "Are we still on for Today", avamessage:"woman-5.jpg"},
+                {id: 3, message: "Cool. It's settled. Tomorrow will discuss the project.", avamessage:"guy-6.jpg"},
+                {id: 4, message: "I suggest a meeting on Tuesday. What do you think?", avamessage:"woman-5.jpg"}
+            ],
+            newMessageText: 'text'
+        },
+        sideBar: {
+            barFriend:
+                [
+                    {id: 1, namefriends: 'Dimitry', imagefriends: 'guy-6.jpg'},
+                    {id: 2, namefriends: 'Mary', imagefriends: 'woman-5.jpg'},
+                    {id: 3, namefriends: 'John', imagefriends: 'guy-4.jpg'}
+                ]
+        }
     },
-    dialogsPage: {
-        dialogsData: [
-            {id: 1, name: 'Dimitry', image: 'guy-6.jpg'},
-            {id: 2, name: 'Mary', image: 'woman-5.jpg'},
-            {id: 3, name: 'John', image: 'guy-4.jpg'},
-            {id: 4, name: 'Ria', image: 'woman-6.jpg'},
-            {id: 5, name: 'Jake', image: 'guy-3.jpg'}
-        ],
-        messages: [
-            {id: 1, message: "Hi Mary, Is it ok if we schedule the meeting tomorrow?", avamessage:"guy-6.jpg"},
-            {id: 2, message: "Are we still on for Today", avamessage:"woman-5.jpg"},
-            {id: 3, message: "Cool. It's settled. Tomorrow will discuss the project.", avamessage:"guy-6.jpg"},
-            {id: 4, message: "I suggest a meeting on Tuesday. What do you think?", avamessage:"woman-5.jpg"}
-        ],
-        newMessageText: 'text'
+    getState() {
+        return this._state;
     },
-    sideBar: {
-        barFriend:
-            [
-                {id: 1, namefriends: 'Dimitry', imagefriends: 'guy-6.jpg'},
-                {id: 2, namefriends: 'Mary', imagefriends: 'woman-5.jpg'},
-                {id: 3, namefriends: 'John', imagefriends: 'guy-4.jpg'}
-            ]
+    callSubscriber() {
+        console.log('State chenged')
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likenum: 0
+        };
+        this._state.profilePage.myPostData.push(newPost);
+        this._state.profilePage.newPostText='';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText=newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
     }
 }
 
-window.state = state;
 
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likenum: 0
-    };
-    state.profilePage.myPostData.push(newPost);
-    state.profilePage.newPostText='';
-    renderEntireTree(state);
-}
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText=newText;
-    renderEntireTree(state);
-}
-
-export  const subscribe = (observer) => {
-    renderEntireTree = observer;
-}
-export default state;
+window.store = store;
+export default store;
 
